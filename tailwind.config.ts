1bc6cbf47d2554a306031cb52afce27c81539301
Muhamad Irvan Dandung <irvandandung1@gmail.com>
2026-01-19 19:34:59 +0700

    style: Update background color to hsl(218 12% 18%)
    - Changed dark-bg from hex #1A1F2E to HSL hsl(218 12% 18%)
    - Improves color precision and matches design specifications

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         primary: '#C778DD',
         'primary-light': '#E8C4F0',
         'primary-dark': '#A65BB8',
-        'dark-bg': '#1A1F2E',
+        'dark-bg': 'hsl(218 12% 18%)',
         'dark-secondary': '#242D3D',
         'dark-tertiary': '#2D3A4F',
       },
